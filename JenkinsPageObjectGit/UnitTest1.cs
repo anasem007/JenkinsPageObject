7c784079bcce9fd57a211166cdeedc248fed90b4
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-05 16:27:21 +0300

    Init

---
+++ JenkinsPageObjectGit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace JenkinsPageObjectGit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
